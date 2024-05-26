13d736cd40f26c46a5cbce0743430d1b405956b8
Alexander <138583718+agarin6@users.noreply.github.com>
2024-05-27 01:24:28 +0300

    done

---
+++ rick-and-morty/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <a href="https://github.com/agarin6">GitHub</a> | <span>&copy; 2023</span>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
